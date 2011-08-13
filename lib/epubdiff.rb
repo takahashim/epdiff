539ca371caabf68e17f99832d76d8a188f47ca87
takahashim <maki@rubycolor.org>
2011-08-13 15:17:59 +0900

    fix Regexp

--- lib/epubdiff.rb
+++ lib/epubdiff.rb
@@ -49,9 +49,9 @@ module Epubdiff
 
       %x(#{unzip_path} #{file1} -d #{tmpdir}/file1)
       %x(#{unzip_path} #{file2} -d #{tmpdir}/file2)
-      p Regexp.escape(tmpdir)
+
       IO.popen("#{diff_path} -r -u #{tmpdir}/file1 #{tmpdir}/file2") do |io|
-        print io.read.gsub(Regexp.escape(tmpdir), "")
+        print io.read.gsub(/#{Regexp.escape(tmpdir)}/, "")
       end
 
     rescue => e
